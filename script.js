1ec5f4953497c1c4716ecab9ec4b5bbc18ea8905
Betül Nur Yıldırım <107762579+BetulBNY@users.noreply.github.com>
2026-01-30 22:46:32 +0300

    Event Listener added to plank

--- script.js
+++ script.js
@@ -42,3 +42,24 @@ function createText(xLocation,yLocation,textColor,fontSize, weight, textAnchor =
     return text;
 }
 
+
+plank.addEventListener('click', function(event) {
+
+// LOCATION PART
+    // Screen position of SVG
+    const rect = svg.getBoundingClientRect();
+    console.log("SVG's screen location:", rect);
+
+    // Calculating x coordinate of the clicked point inside SVG
+    // (Real mouse coordiantes - Left side of the SVG (distance between frame and left of screen))
+    const mouseX = event.clientX - rect.left;
+    console.log("SVG left:", rect.left);
+    console.log("Mouse click coordinates:", event.clientX);
+
+    // Distance from the pivot(center)
+    const distance = mouseX - 400;
+
+    console.log("X inside SVG:", mouseX.toFixed(0));
+    console.log("Distance from center:", distance.toFixed(0));}
+
+
